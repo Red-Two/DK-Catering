8cbb80f93e562459e21079d0df8e71b5b45a6ad2
rowen-foss <caesar.severus@gmail.com>
2015-05-09 18:11:23 -0400

    added some nav stuff

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -8,10 +8,40 @@
 
 		<ul id="navTabs">
 			<li><a href="index.html">Home</a></li>
-			<li><a href="floor1.html">Floor 1</a></li>
-			<li><a href="floor2.html">Floor 2</a></li>
-			<li><a href="floor3.html">Floor 3</a></li>
-			<li><a href="outside.html">Outside</a></li>
+			<li>Menu
+			    <ul>
+			        <li><a href="<?php dataInclusion("menu/breakfast.php") ?>">Breakfast</a></li>
+				<li><a href="<?php dataInclusion("menu.lunch.php") ?>">Lunch</a></li>
+				<li><a href="<?php dataInclusion("menu.dinner.php") ?>"">Dinner</a></li>
+				<li><a href="<?php dataInclusion("menu.desert.php") ?>"">Desert</a></li>
+				<li><a href="<?php dataInclusion("menu.platters.php") ?>"">Platters</a></li>
+			    </ul>
+			</li>
+			<li>Catering
+			    <ul>
+			        <li><a href="<?php dataInclusion("menu.events.php") ?>"">Events</a></li>
+                                <li><a href="<?php dataInclusion("menu.testimonials.php") ?>"">Testimonials</a></li>
+                                <li><a href="<?php dataInclusion("menu.photos.php") ?>"">Photos</a></li>
+			    </ul>
+			</li>
+			<li>Recipes
+			    <ul>
+			         <li><a href="<?php dataInclusion("menu.ours.php") ?>"">Our Recipes</a></li>
+                                 <li><a href="<?php dataInclusion("menu.yours.php") ?>"">Your Recipes</a></li>
+			    </ul>
+			</li>
+			<li>Community
+			    <ul>
+			         <li><a href="<?php dataInclusion("menu.forums.php") ?>"">Forums</a></li>
+                                 <li><a href="<?php dataInclusion("menu.reviews.php") ?>"">Reviews</a></li>
+			    </ul>
+			</li>
+			<li>About
+			    <ul>
+			     <li><a href="<?php dataInclusion("menu.lunch.php") ?>"">Our Story</a></li>
+                             <li><a href="<?php dataInclusion("menu.lunch.php" ?>"">Contact Us</a></li>
+			    </ul>
+			</li>        
 		</ul>
 	</nav>
 </header>
