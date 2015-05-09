2530b6efdc892470fd71c3296f6d06121c3aca51
Ellison patterson <silence@xenogamers.org>
2015-05-09 16:14:22 -0400

    Fixed Mixed-Content Warning

--- assets/templates/head.php
+++ assets/templates/head.php
@@ -2,6 +2,6 @@
 	<meta charset='utf-8' />
 	<title><?php echo getName(); echo " - "; echo getPageTitle(); ?></title>
 
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" type="text/css" href="<?php echo dataInclusion("assets/css/style.css"); ?>">
 </head>
