71113aa1429b704e0c155a1c407782bcc5d7eabe
Ellison patterson <silence@xenogamers.org>
2015-05-11 18:56:45 -0400

    Removed old files

--- assets/templates/billboard.php
+++
@@ -1,5 +0,0 @@
-<?php $template = new Template(); ?>
-
-<div id="billboard">
-	<img id="<?php echo $template->getBBItem(); ?>" src="<?php echo $template->dataInclusion("assets/images/clear.png"); ?>" alt="clear" />
-</div>
--- assets/templates/head.php
+++
@@ -1,9 +0,0 @@
-<?php $template = new Template(); ?>
-
-<head>
-	<meta charset='utf-8' />
-	<title><?php echo $template->getName(); echo " - "; echo $template->getPageTitle(); ?></title>
-
-	<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-	<link rel="stylesheet" type="text/css" href="<?php echo $template->dataInclusion("assets/css/style.css"); ?>">
-</head>
